e2bd036c380e075b7ef73d030d3c1752f4985643
Frank Lockerman <motym123@gmail.com>
2014-06-02 14:00:09 -0400

    Optimization tweaks and a code cleanup.

--- verseapp.js
+++ verseapp.js
@@ -1,17 +1,11 @@
-/******************
+/*******************************
  * Global Variables
- ******************/
-var currentVerse = 1; //************DOES THIS GET USED?*****************
+ *******************************/
 var json;
-var maxVerse; //******** DOES THIS GET USED??***********************
 
-//Check for Quizlet Mode
-if (localStorage.quizlet == "true") {
-	var quizlet = true;
-} else {
-	var quizlet = false;
-};
-
+/*******************************
+ * Helper Prototypes
+ *******************************/
 
 Storage.prototype.setObject = function (key, value) {
 	this.setItem(key, JSON.stringify(value));
@@ -23,33 +17,48 @@ Storage.prototype.getObject = function (key) {
 }
 
 /******************************
- *  Load Verses from the Server.
- *   - location: URL that the JSON is coming from.
- *	 - storageName: localStorage name to store the JSON under.
+ *  function loadVerses()
+ *  Load Verses from the Server and populate the screen.
+ *   = location: URL that the JSON is coming from.
+ *	 = storageName: localStorage name to store the JSON under.
  ******************************/
 
 function loadVerses(location, storageName) {
 	$.ajax({
 		url: location,
-		dataType: "text", //force to handle it as text
+		dataType: "text",
 		success: function (data) {
 			json = JSON.parse(data);
 			localStorage.setObject(storageName, json);
-			/*if (quizlet) {
+
+			//Enable when Quizlet Integration is added
+
+			/*if (localStorage.quizlet == "true") {
 				quizlet_populate(currentVerse);
 			} else {*/
-			populate(storageName);
 
+			populate(storageName);
 		}
 	})
 }
 
+
 /******************************
- *
- *
+ * function fixHeight()
+ * Redraws the interface after calculating the exact measurments. Guarentees a beautiful interface on every device.
+ ******************************/
+function fixHeight() {
+	var newHeight = window.innerHeight - 50 - $("#knownButton").height() - 26.22 + "px"; //subtract the header, nav size, and padding for nav
+	$(".versecontainer").css("height", newHeight);
+}
+
+/******************************
+ * function populate()
+ * Parse the JSON object and populate the app with data.
+ *   = storageName: name of localStorage array to load
  ******************************/
 function populate(storageName) {
-	if (localStorage[storageName]) {
+	if (localStorage[storageName]) { //Check that the data loaded properly
 		jsonObject = JSON.parse(localStorage[storageName]);
 
 		//generate the verses for this pack
@@ -57,7 +66,7 @@ function populate(storageName) {
 			$("#versecontainer").append('<div class="verse" data-role="collapsible" data-collapsed-icon="" data-expanded-icon="" data-inset="false" style="margin: 0px;"><h4><center>' + jsonObject.verse[key].reference + '</center><a onclick=""><img src="assets/menu.png" class="menu" /></a></h4><p>' + jsonObject.verse[key].text + '</p></div>').trigger('create');
 		}
 
-		// change the pack name
+		// Update the pack name on the screen
 		$("#header").html(jsonObject.packName);
 
 		//Check if the user is authorized to add verses to this pack
@@ -66,13 +75,20 @@ function populate(storageName) {
 		} else {
 
 		}
-		fixHeight();
 
-	} else {
-		alert("There appears to be no data loaded. Please restart the app."); //*******MAKE MORE USER FRIENDLY*******
+		fixHeight(); //redraw the interface
+
+	} else { //Catch the error if the data is missing. Alert the user.
+		alert("There appears to be no data loaded. Please restart the app and try again.");
 	}
 }
 
+
+/*************************************
+ * function addVerse()
+ * Adds a verse to the verse pack
+     = storageName: localStorage name to store the new data under.
+ *************************************/
 function addVerse(storageName) {
 	if (localStorage[storageName]) {
 		var json = JSON.parse(localStorage[storageName])
