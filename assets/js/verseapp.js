a7740035c707202aa96743a0da76ea245cbb7038
Frank Lockerman <motym123@gmail.com>
2014-06-26 10:45:25 -0400

    Commit before app logic changes. Just for the sake of saving

--- assets/js/verseapp.js
+++ assets/js/verseapp.js
@@ -7,7 +7,7 @@ var verseNumber; //used to easily pass "id" numbers for functions
 var debug = false; //turn on debugging
 
 //configuration
-var stockPack = "SummerPack.json"; //url to the stock pack that comes with the app
+var stockPack = "assets/SummerPack.json"; //url to the stock pack that comes with the app
 
 /*******************************
  * Helper Prototypes
@@ -66,7 +66,9 @@ function fixHeight() {
  * function populate()
  * Parse the JSON object and populate the app with data.
  *   = storageName: name of localStorage array to load
- *   = packSectionName: which section of the pack should be loaded?
+ *   = packSectionName: which section of the pack should be loaded
+ *
+ * TODO: potentially eliminate packSectionName due to localStorage.currentSection
  ******************************/
 
 function populate(storageName, packSectionName) {
@@ -75,7 +77,7 @@ function populate(storageName, packSectionName) {
 
 		//generate the verses for this pack
 		for (var key in jsonObject[packSectionName]) {
-			$("#versecontainer").append('<div class="verse" data-role="collapsible" data-collapsed-icon="" data-expanded-icon="" data-inset="false" style="margin: 0px;"><h4><center>' + jsonObject[packSectionName][key].reference + '</center><a onclick="toggleVerseMenu(' + jsonObject[packSectionName][key].id + '); "><img src="assets/menu.png" class="menu" /></a></h4><p>' + jsonObject[packSectionName][key].text + '</p></div>').trigger('create');
+			$("#versecontainer").append('<div class="verse" data-role="collapsible" data-collapsed-icon="" data-expanded-icon="" data-inset="false" style="margin: 0px;"><h4><center>' + jsonObject[packSectionName][key].reference + ' (' + jsonObject[packSectionName][key].version + ')</center><a onclick="toggleVerseMenu(' + jsonObject[packSectionName][key].id + '); "><img src="assets/img/menu.png" class="menu" /></a></h4><p>' + jsonObject[packSectionName][key].text + '</p></div>').trigger('create');
 		}
 
 		// Update the pack name on the screen
@@ -92,6 +94,7 @@ function populate(storageName, packSectionName) {
 		}
 
 		fixHeight(); //redraw the interface
+		addSelected();
 
 	} else { //Catch the error if the data is missing. Alert the user.
 		alert("There appears to be no data loaded. Please restart the app and try again.");
@@ -108,6 +111,7 @@ function initializeApp() {
 
 	// verify required settings are present
 	if (!localStorage["packList"]) {
+		makeUserProfile();
 		$.ajax({
 			url: stockPack,
 			dataType: "text",
@@ -119,8 +123,8 @@ function initializeApp() {
 				// Create the pack list default settings
 				localStorage.packList = '{"defaultPack":"' + systemname + '","packs": [{"userName": "' + json.packName + '","systemName": "' + systemname + '","id":"1"}]}';
 				localStorage["currentPack"] = systemname;
-				localStorage["currentSection"] = "working";
-				populate(systemname, "working"); //update users screen
+				localStorage["currentSection"] = "other";
+				populate(systemname, "other"); //update users screen
 			}
 		})
 	} else { //required files exist, proceed with display
@@ -128,16 +132,17 @@ function initializeApp() {
 		//process a pack change
 		if (localStorage["changePack"]) {
 			if (debug) {
-				console.log("Pack changed to " + localStorage["changePack"]);
+				console.log("Pack changed to " + localStorage.changePack);
 			}
-			populate(localStorage["changePack"], localStorage["currentSection"]);
-			localStorage.currentPack = localStorage["changePack"];
+			populate(localStorage.changePack, localStorage.currentSection);
+			localStorage.currentPack = localStorage.changePack;
 			localStorage.removeItem("changePack");
 
 		}
 		// process the current pack selected
 		else if (localStorage["currentPack"]) {
 			populate(localStorage["currentPack"], localStorage["currentSection"]);
+
 		}
 		// if this is not first run and the user has not added anything that needs processing, process the default pack
 		else {
@@ -170,27 +175,101 @@ function clearScreen() {
 }
 
 
+/********************************************
+ * function addSelected()
+ * Visually displays the current pack section to the user by changing the color of the button
+ ********************************************/
+function addSelected() {
+	if (localStorage.currentSection == "other") {
+		$("#allButton").addClass("selected");
+	} else {
+		$("#" + localStorage.currentSection + "Button").addClass("selected");
+	}
+}
+
+
+/********************************************
+ * function removeSelected()
+ * Removes the background that is added from addSelected()
+ ********************************************/
+function removeSelected() {
+	if (localStorage.currentSection == "other") {
+		$("#allButton").removeClass("selected");
+	} else {
+
+		$("#" + localStorage.currentSection + "Button").removeClass("selected");
+	}
+
+
+}
 /****************************************
  * function removeVerse()
  * Removes a verse a pack
-   = storageName: name of the pack being edited
-	 = packSectionName: name of the section being edited
-	 = idNumber: "id" of verse that is being edited
+ *  = storageName: name of the pack being edited
+ *  = packSectionName: name of the section being edited
+ *  = idNumber: "id" of verse that is being edited
  ****************************************/
 
 function removeVerse(storageName, packSectionName, idNumber) {
 	//prevent accidental deletion
-	var confirm = window.confirm("Are you sure you want to delete this verse?");
-	if (confirm) {
+	if (confirm("Are you sure you want to delete this verse?") == true) {
 		var jsonObject = JSON.parse(localStorage[storageName]);
 		for (var key in jsonObject[packSectionName]) {
 			if (jsonObject[packSectionName][key].id == idNumber) {
 				jsonObject[packSectionName].splice(key, 1);
 			} else continue
 		}
-
 		localStorage[storageName] = JSON.stringify(jsonObject);
 		clearScreen();
 		populate(storageName, packSectionName);
 	}
+}
+
+/******************************************
+ * function moveVerse()
+ * Moves a verse between sections in a pack
+ *  = location: the name of the section to move the verse to
+ *
+ * TODO: this can be optimized with array manupulation instead of the holder var
+ ******************************************/
+
+function moveVerse(location) {
+	var pack = localStorage.currentPack;
+	var section = localStorage.currentSection;
+	var jsonObject = JSON.parse(localStorage[pack]); //get the pack
+	for (var key in jsonObject[section]) {
+		if (jsonObject[section][key].id == verseNumber) { //find the right verse
+			jsonObject[location].push({
+				"text": jsonObject[section][key].text,
+				"reference": jsonObject[section][key].reference,
+				"id": jsonObject[location].length + 1,
+				"version": jsonObject[section][key].version
+			});
+			jsonObject[section].splice(key, 1); //remove the old data
+			localStorage[pack] = JSON.stringify(jsonObject);
+			clearScreen();
+			populate(pack, section);
+		}
+	}
+}
+
+
+/********************************************
+ * function changeSection()
+ * Changes the section in the current pack
+ *  = section: name of section to change to
+ ********************************************/
+
+function changeSection(section) {
+	removeSelected();
+	localStorage.currentSection = section;
+	clearScreen();
+	populate(localStorage.currentPack, section);
+
+}
+
+function makeUserProfile() {
+	var d = new Date();
+	var uuid = d.getTime() + Math.uuid(10, 16);
+	localStorage.userProfile = '{"uuid": "' + uuid + '","userProfile":"","userSettings": [{"moveToKnown": "","testStyle": ""}]}';
 }
